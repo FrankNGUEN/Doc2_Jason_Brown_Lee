831b0feb08b7f8f6b6fe340b67d2faf695504a9d
DUC Nguyen-Manh <nguyenmanhduc18@gmail.com>
2022-03-27 23:43:53 +0900

    Add files via upload

---
+++ gan/multilayer_perceptron.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  1 23:21:18 2022
+
+@author: HaiDuc
+"""
+
+# example of a multilayer perceptron
+#from keras.utils import plot_model
+from keras.models import Model
+from keras.layers import Input
+from keras.layers import Dense
+
+visible = Input(shape=(10,))
+hidden1 = Dense(10, activation='relu')(visible)
+hidden2 = Dense(20, activation='relu')(hidden1)
+hidden3 = Dense(10, activation='relu')(hidden2)
+output  = Dense(1, activation='sigmoid')(hidden3)
+
+model = Model(inputs=visible, outputs=output)
+# summarize layers
+model.summary()
+# plot graph
+#plot_model(model, to_file='multilayer_perceptron_graph.png')
---
+++ gan/rnn.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  1 23:58:14 2022
+
+@author: HaiDuc
+"""
+
+# example of a recurrent neural network
+#from keras.utils import plot_model
+from keras.models import Model
+from keras.layers import Input
+from keras.layers import Dense
+from keras.layers.recurrent import LSTM
+visible = Input(shape=(100,1))
+hidden1 = LSTM(10)(visible)
+hidden2 = Dense(10, activation='relu')(hidden1)
+output = Dense(1, activation='sigmoid')(hidden2)
+model = Model(inputs=visible, outputs=output)
+# summarize layers
+model.summary()
+# plot graph
+#plot_model(model, to_file='recurrent_neural_network.png')
